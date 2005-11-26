f982ea1a4133fa263a525a39813d3eface001da5
Nicholas Piper <nicholas@users.sourceforge.net>
2005-11-26 14:31:15 +0000

    Add LGPL messages
    git-svn-id: https://gtkpod.svn.sourceforge.net/svnroot/gtkpod/libgpod/trunk@1175 f01d2545-417e-4e96-918e-98f8d0dbbcb6

--- bindings/python/examples/play.py
+++ bindings/python/examples/play.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
 
+##  Copyright (C) 2005 Nick Piper <nick-gtkpod at nickpiper co uk>
+##  Part of the gtkpod project.
+ 
+##  URL: http://www.gtkpod.org/
+##  URL: http://gtkpod.sourceforge.net/
+
+##  The code contained in this file is free software; you can redistribute
+##  it and/or modify it under the terms of the GNU Lesser General Public
+##  License as published by the Free Software Foundation; either version
+##  2.1 of the License, or (at your option) any later version.
+
+##  This file is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+##  Lesser General Public License for more details.
+
+##  You should have received a copy of the GNU Lesser General Public
+##  License along with this code; if not, write to the Free Software
+##  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+##  $Id$
+
+
+# this file is just a little example I use for testing, it doesn't
+# play the music, but plays with the database ;-)
+
 import os, os.path
 import gpod
 import sys
