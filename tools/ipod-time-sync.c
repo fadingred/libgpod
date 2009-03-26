231893c2e0656613772bf510244e4ebfa32795ec
Christophe Fergeau <teuf@gnome.org>
2009-03-26 22:11:27 +0000

    Add test program for iPod time sync
    git-svn-id: https://gtkpod.svn.sourceforge.net/svnroot/gtkpod/libgpod/trunk@2286 f01d2545-417e-4e96-918e-98f8d0dbbcb6

---
+++ tools/ipod-time-sync.c
@@ -0,0 +1,42 @@
+/* Copyright (c) 2009, Christophe Fergeau  <teuf@gnome.org>
+ *
+ * The code contained in this file is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * iTunes and iPod are trademarks of Apple
+ *
+ * This product is not supported/written/published by Apple!
+ *
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib.h>
+#include <glib/gi18n.h>
+
+extern void sync_time (const char *device, time_t current_time);
+
+int
+main (int argc, char **argv)
+{
+    if (argc < 2) {
+        g_print (_("usage: %s <device>\n"), g_basename (argv[0]));
+	return 1;
+    }
+
+    sync_time (argv[1], time (NULL));
+
+    return 0;
+}
