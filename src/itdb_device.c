f9ba86b10ba59f10864a817ac8c949a747eb40d3
Christophe Fergeau <teuf@gnome.org>
2010-12-28 13:00:51 +0100

    Add 16GB Black Nano 6g serial number
    Courtesy of J. Alves

--- src/itdb_device.c
+++ src/itdb_device.c
@@ -831,6 +831,7 @@ static const ItdbSerialToModel serial_to_model_mapping[] = {
     { "DW1", "C691" }, /* 8GB Orange Nano 6g */
     { "DW2", "C692" }, /* 8GB Pink Nano 6g */
     { "CMP", "C526" }, /* 16GB Silver Nano 6g */
+    { "DW4", "C694" }, /* 16GB Black Nano 6g */
     { "DW5", "C695" }, /* 16GB Blue Nano 6g */
     { "DW9", "C699" }, /* 16GB Red Nano 6g */
 
