e33da3d8a15e201681b563f0ea3f827eb278deba
Christophe Fergeau <teuf@gnome.org>
2011-01-19 23:01:02 +0100

    add more nano6g serial numbers
    I got the Green 8GB and Pink 16GB from a store, and inferred the
    Green and Orange 16GB as well as the 8GB Red serial numbers from
    there.

--- src/itdb_device.c
+++ src/itdb_device.c
@@ -828,11 +828,16 @@ static const ItdbSerialToModel serial_to_model_mapping[] = {
     { "CMN", "C525" }, /* 8GB Silver Nano 6g */
     { "DVX", "C688" }, /* 8GB Black Nano 6g */
     { "DVY", "C689" }, /* 8GB Blue Nano 6g */
+    { "DW0", "C690" }, /* 8GB Green Nano 6g */
     { "DW1", "C691" }, /* 8GB Orange Nano 6g */
     { "DW2", "C692" }, /* 8GB Pink Nano 6g */
+    { "DW3", "C693" }, /* 8GB Pink Nano 6g */
     { "CMP", "C526" }, /* 16GB Silver Nano 6g */
     { "DW4", "C694" }, /* 16GB Black Nano 6g */
     { "DW5", "C695" }, /* 16GB Blue Nano 6g */
+    { "DW6", "C696" }, /* 16GB Green Nano 6g */
+    { "DW7", "C697" }, /* 16GB Orange Nano 6g */
+    { "DW8", "C698" }, /* 16GB Pink Nano 6g */
     { "DW9", "C699" }, /* 16GB Red Nano 6g */
 
     { "A1S", "C306" }, /* 2GB Silver Shuffle 3g */
